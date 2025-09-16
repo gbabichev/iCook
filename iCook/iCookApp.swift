909b51ea768b649ffd9b35b460fd52f2da7ef5b4
George Babichev <gbabichev@xantrion.com>
2025-09-16 10:32:51 -0400

    Initial Commit

---
+++ iCook/iCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCookApp.swift
+//  iCook
+//
+//  Created by George Babichev on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct iCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
